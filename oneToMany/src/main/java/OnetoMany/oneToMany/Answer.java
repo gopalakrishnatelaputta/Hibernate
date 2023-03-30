7d2433518226bc12a5aecc27491ae5e3ed497ccd
gopalakrishnatelaputta <121857875+gopalakrishnatelaputta@users.noreply.github.com>
2023-03-30 16:44:06 +0530

    Add files via upload

---
+++ oneToMany/src/main/java/OnetoMany/oneToMany/Answer.java
@@ -0,0 +1,48 @@
+package OnetoMany.oneToMany;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="anss")
+class Answer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.TABLE)
+	
+	private int id;
+	private String answername;
+	private String postedBy;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getAnswername() {
+		return answername;
+	}
+	public void setAnswername(String answername) {
+		this.answername = answername;
+	}
+	public String getPostedBy() {
+		return postedBy;
+	}
+	public void setPostedBy(String postedBy) {
+		this.postedBy = postedBy;
+	}
+	public Answer(int id, String answername, String postedBy) {
+		super();
+		this.id = id;
+		this.answername = answername;
+		this.postedBy = postedBy;
+	}
+	public Answer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
